be9d70cfeffffdb8995026eae0164fbc128e12d9
Laura Hoffman <lhoffmanwg@gmail.com>
2018-11-15 01:36:52 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -1,29 +1,28 @@
-
-function theBeatlesPlay(musicians, instruments){
-var emptyArray = [];
-for (var i = 0; i < musicians.length; i++) {
-  emptyArray.push(musicians[i] + " plays " + instruments[i]);
- }
- return emptyArray;
+function theBeatlesPlay(musicians, instruments) {
+  var i;
+  var emptyArray = [];
+  for(i=0; i<musicians.length; i++) {
+    emptyArray.push(musicians[i] + " plays " + instruments[i]);
+  } 
+  return emptyArray;
 }
 
-function johnLennonFacts(facts){
+function johnLennonFacts(facts) {
   var i = 0;
   var emptyArray = [];
-  while (i < facts.length){
+  while(i < facts.length) {
     emptyArray.push(facts[i] + "!!!");
-    i = i + 1;
+    i = i+1
   }
   return emptyArray;
 }
 
-function iLoveTheBeatles(n) {
+function iLoveTheBeatles(number) {
+  
   var emptyArray = [];
   do {
-     emptyArray.push("I love the Beatles!");
-     n = n + 1;
-  } while (n < 15);
-  
-  return emptyArray;
-  
+    emptyArray.push("I love the Beatles!");
+    number = number + 1;
+  } while(number < 15) 
+    return emptyArray;
 }
